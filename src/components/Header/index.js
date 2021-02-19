c618cabbbbae186d7d82697490256f224fe5bedd
Ana Carolina Fidelis Gonçalves <acarolinafg@gmail.com>
2021-02-18 21:25:48 -0300

    Criação do componente de Header de navegação e criação do arquivo Env .js para constantes do sistema

---
+++ src/components/Header/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Container, Navbar } from 'react-bootstrap';
+
+import Env from '../../includes/Env';
+import logo from '../../assets/images/logo72.png';
+
+export default function () {
+  return (
+    <Navbar
+      className="flex-md-nowrap p-1 shadow"
+      bg="primary"
+      expand="md"
+      variant="dark"
+      role="navigation"
+    >
+      <Container fluid>
+        <Navbar.Brand href="/" className="text-uppercase pl-2 pl-md-0">
+          <img
+            src={logo}
+            className="d-inline-block align-top"
+            height={30}
+            width={30}
+            alt={Env.name}
+          />
+          &nbsp;&nbsp;{Env.name}
+        </Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
+}
---
+++ src/includes/Env.js
@@ -0,0 +1,14 @@
+const Env = {
+  name: 'Brasil Aberto',
+  description:
+    'Aplicativo de análise dos dados abertos governamentais do Brasil',
+  copy: 'Brasil Aberto 2021',
+  colors: {
+    blue: '#28166f',
+    yellow: '#f8c300',
+    green: '#00923f',
+  },
+  version: '0.1.0',
+};
+
+export default Env;
