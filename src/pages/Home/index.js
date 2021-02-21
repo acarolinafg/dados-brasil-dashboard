22dad66380049d9eb76b6bfb906acf576e44d35a
Ana Carolina Fidelis Gonçalves <acarolinafg@gmail.com>
2021-02-21 17:48:24 -0300

    Troca do componente footer na Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,19 +1,7 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import Env from '../../includes/Env';
-
-function Footer() {
-  return (
-    <footer className="footer bg-success" role="contentinfo">
-      <Container fluid>
-        <Row className="text-white">
-          <Col>&copy; {Env.copy}</Col>
-          <Col className="text-right">Versão {Env.version}</Col>
-        </Row>
-      </Container>
-    </footer>
-  );
-}
+import Footer from '../../components/Footer';
 
 export default function Home() {
   return (
