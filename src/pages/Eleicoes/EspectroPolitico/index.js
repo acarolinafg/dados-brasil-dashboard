32194ef72a075b4aa0751a826c51823fc6e80def
Ana Carolina Fidelis Gonçalves <acarolinafg@gmail.com>
2021-02-22 05:04:28 -0300

    Inclusão do formulário nas páginas

--- src/pages/Eleicoes/EspectroPolitico/index.js
+++ src/pages/Eleicoes/EspectroPolitico/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import Dashboard from '../../../components/Dashboard';
+import SearchForm from '../../../components/Search/Form/Eleicoes';
 
 export default function EspectroPolitico() {
   const navigation = [
     { key: 1, name: 'Eleições' },
     { key: 2, name: 'Espectro Político', active: true },
   ];
+  const sidebar = <SearchForm />;
   return (
     <Dashboard
       title="Espectro Político"
       icon={['fas', 'flag-checkered']}
       navigation={navigation}
+      sidebar={sidebar}
     />
   );
 }
--- src/pages/Eleicoes/Partidos/index.js
+++ src/pages/Eleicoes/Partidos/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import Dashboard from '../../../components/Dashboard';
+import SearchForm from '../../../components/Search/Form/Eleicoes';
 
 export default function Partidos() {
   const navigation = [
     { key: 1, name: 'Eleições' },
     { key: 2, name: 'Partidos', active: true },
   ];
-
+  const sidebar = <SearchForm />;
   return (
     <Dashboard
       title="Partidos"
       icon={['fas', 'flag']}
       navigation={navigation}
+      sidebar={sidebar}
     />
   );
 }
